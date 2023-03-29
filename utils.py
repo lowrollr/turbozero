5d7ac94404d95b8bd6b94fa18996e1f433035dc5
lowrollr <92640744+lowrollr@users.noreply.github.com>
2023-03-29 10:17:11 -0700

    fix board representation

--- utils.py
+++ utils.py
@@ -4,13 +4,16 @@ import numpy as np
 def input_to_tensor(board_states):
     # convert board state values to one-hot encodings
     board_state = np.stack(board_states)
-    one_hotified = np.eye(18)[board_state].reshape(-1, 18, 4, 4)
+    one_hotified = np.eye(18)[board_state]
     tensor = torch.from_numpy(one_hotified).float()
     return tensor
 
-
-def input_to_tensor_3d(board_states):
-    board_state = np.stack(board_states)
-    one_hotified = np.eye(18)[board_state].reshape(-1, 1, 18, 4, 4)
-    tensor = torch.from_numpy(one_hotified).float()
+# encodes board states as one-hot tensors
+# 4 x 4 board state -> 18 x 4 x 4 tensor
+def input_to_tensor_3d(board_states, num_classes=18):
+    # convert board state values to one-hot encodings
+    board_state = np.stack(board_states, axis=0)
+    one_hotified = np.eye(num_classes)[board_state].transpose(0, 3, 1,2)
+    tensor = torch.from_numpy(one_hotified).float().unsqueeze(1)
+    # add a dimension to the front
     return tensor
