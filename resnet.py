18f817df590dd569d1314a23acae04d20ea0d9de
lowrollr <92640744+lowrollr@users.noreply.github.com>
2023-03-29 22:25:46 -0700

    resnet model

---
+++ resnet.py
@@ -0,0 +1,66 @@
+import torch.nn as nn
+import torch
+
+# https://blog.paperspace.com/writing-resnet-from-scratch-in-pytorch/
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride = 1, downsample = None):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Sequential(
+                        nn.Conv2d(in_channels, out_channels, kernel_size = 2, stride = stride, padding = 'same'),
+                        nn.BatchNorm2d(out_channels),
+                        nn.ReLU())
+        self.conv2 = nn.Sequential(
+                        nn.Conv2d(out_channels, out_channels, kernel_size = 2, stride = 1, padding = 'same'),
+                        nn.BatchNorm2d(out_channels))
+        self.downsample = downsample
+        self.relu = nn.ReLU()
+        self.out_channels = out_channels
+        
+    def forward(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.conv2(out)
+        if self.downsample:
+            residual = self.downsample(x)
+        out += residual
+        out = self.relu(out)
+        return out
+    
+class ResNet2Heads(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.conv_block = nn.Sequential(
+            nn.Conv2d(18, 64, kernel_size=(2,2), stride=1, padding='same'), # 1 x 19 x 4 x 4 -> 16 x 18 x 3 x 3
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+        )
+        self.res_blocks = nn.Sequential(
+            ResidualBlock(64, 64),
+            ResidualBlock(64, 64),
+            ResidualBlock(64, 64),
+            ResidualBlock(64, 64)
+        )
+
+        self.connector = nn.Sequential(
+            nn.Flatten(start_dim=1),
+            nn.Linear(64 * 4 * 4, 64),
+            nn.ReLU()
+        )
+
+        self.policy_head = nn.Sequential(
+            nn.Linear(64, 4),
+            nn.Softmax(dim=1)
+        )
+
+        self.value_head = nn.Sequential(
+            nn.Linear(64, 1),
+        )
+            
+
+    def forward(self, x):
+        x = self.conv_block(x)
+        x = self.res_blocks(x)
+        x = self.connector(x)
+        policy = self.policy_head(x)
+        value = self.value_head(x)
+        return policy, value
