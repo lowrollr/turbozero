bfc965188f8e578e2babc90b88a5c7fc029844e5
lowrollr <92640744+lowrollr@users.noreply.github.com>
2023-04-03 18:55:37 -0700

    alternate representation

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+import numba
 
 def input_to_tensor(board_states):
     # convert board state values to one-hot encodings
@@ -17,3 +18,33 @@ def input_to_tensor_3d(board_states, num_classes=18):
     tensor = torch.from_numpy(one_hotified).float().unsqueeze(1)
     # add a dimension to the front
     return tensor
+
+def input_to_tensor_scalar(board_states):
+    tensors = []
+    for board in board_states:
+        board = np.array(board)
+        tensor = torch.stack([
+            torch.from_numpy(np.equal(board, 0)).float(),
+            *torch.from_numpy(np.stack(compare_tiles(board), axis=0)).float(),
+            torch.from_numpy(board).float()
+        ], dim=0)
+        tensors.append(tensor)
+    return torch.stack(tensors, dim=0)
+
+    
+
+# @numba.njit(nogil=True, fastmath=True)
+def compare_tiles(arr): # thanks GPT-4!
+    # Compare with the tile below
+    shifted_down = np.roll(arr, -1, axis=0)
+    vertical_comparison = np.equal(arr, shifted_down).astype(float)
+    vertical_comparison[:-1, :] = vertical_comparison[1:, :]
+    vertical_comparison[-1, :] = 0
+
+    # Compare with the tile to the right
+    shifted_right = np.roll(arr, -1, axis=1)
+    horizontal_comparison = np.equal(arr, shifted_right).astype(float)
+    horizontal_comparison[:, :-1] = horizontal_comparison[:, 1:]
+    horizontal_comparison[:, -1] = 0
+
+    return vertical_comparison, horizontal_comparison
