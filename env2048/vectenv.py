5e27fd23c968db2fc59a7deec9277ac226a7b81a
lowrollr <92640744+lowrollr@users.noreply.github.com>
2023-05-29 14:58:36 -0700

    more vec

--- env2048/vectenv.py
+++ env2048/vectenv.py
@@ -5,8 +5,8 @@ import torch
 class Vectorized2048Env:
     def __init__(self, num_parallel_envs, device):
         self.num_parallel_envs = num_parallel_envs
-        self.boards = torch.zeros((num_parallel_envs, 4, 4), dtype=torch.int32, device=device)
-        self.reset_mask = torch.zeros(num_parallel_envs, dtype=torch.bool, device=device)
+        self.boards = torch.zeros((num_parallel_envs, 1, 4, 4), dtype=torch.int16, device=device)
+        self.valid_mask = torch.ones((num_parallel_envs, 1, 1, 1), dtype=torch.bool, device=device)
         self.mcts_trees = [None for _ in range(num_parallel_envs)]
         self.device = device
         self.very_negative_value = -1e5
@@ -19,15 +19,27 @@ class Vectorized2048Env:
     def reset(self, seed=None) -> None:
         if seed is not None:
             torch.manual_seed(seed)
-        
-    def step(self, actions: torch.Tensor):
-        self.push_moves(actions)
-        self.reset_mask = self.check_terminal()
+        self.boards.fill_(0)
+        self.valid_mask.fill_(0)
+        self.spawn_tiles()
         self.spawn_tiles()
         
+    def step(self, actions: torch.Tensor) -> torch.Tensor:
+        self.push_moves(actions)
+        self.update_valid_mask()
+        terminated_boards = torch.logical_not(self.valid_mask.clone())
+        self.reset_invalid_boards()
+        self.spawn_tiles()
+        return terminated_boards
+    
+    def reset_invalid_boards(self):
+        self.boards *= self.valid_mask.view(self.num_parallel_envs, 1, 1, 1)
+        self.spawn_tiles(torch.logical_not(self.valid_mask))
+        self.valid_mask.fill_(1)
+        
 
-    def check_terminal(self) -> torch.Tensor:
-        return self.get_legal_moves().sum(dim=1) == 0
+    def update_valid_mask(self) -> None:
+        self.valid_mask *= (self.get_legal_moves().sum(dim=1, keepdim=True) > 0).view(-1, 1, 1, 1)
 
     def get_legal_moves(self) -> torch.Tensor:
         # check for empty spaces
@@ -68,11 +80,14 @@ class Vectorized2048Env:
         probs = self.base_probabilities * valid_progressions.view(self.num_parallel_envs, 32)
         return progressions, probs
 
-    def spawn_tiles(self) -> None:
+    def spawn_tiles(self, mask=None) -> None:
         progs, probs = self.get_progressions()
         probs += torch.where(probs.amax(dim=1, keepdim=True) == 0, torch.full_like(probs, 1), torch.full_like(probs, 0))
         indices = torch.multinomial(probs, 1)
-        self.boards = progs[(range(self.num_parallel_envs), indices[:,0])]
+        if mask is not None:
+            self.boards = torch.where(mask, progs[(range(self.num_parallel_envs), indices[:,0])].unsqueeze(1), self.boards)
+        else:
+            self.boards = progs[(range(self.num_parallel_envs), indices[:,0])].unsqueeze(1)
     
     def rotate_by_amnts(self, amnts) -> None:
         rotations_0 = self.boards
