38c8faef7191dcb64dd2c8d96a5eb9fdb3720028
lowrollr <92640744+lowrollr@users.noreply.github.com>
2023-06-06 12:28:43 -0700

    fix

--- env2048/vectmcts.py
+++ env2048/vectmcts.py
@@ -25,7 +25,7 @@ class Vectorized2048MCTSLazy:
 
         q_values = torch.where(self.visits > 0, self.action_scores / self.visits, self.very_positive_value)
         puct_scores = q_values + (self.puct_c * probs * torch.sqrt(n_sum + 1) / (1 + self.visits))
-        legal_action_scores = puct_scores - (self.very_positive_value * (1 - legal_actions))
+        legal_action_scores = puct_scores - (self.very_positive_value * torch.logical_not(legal_actions))
         chosen_actions = torch.argmax(legal_action_scores, dim=1, keepdim=True)
         return chosen_actions.squeeze(1)
 
