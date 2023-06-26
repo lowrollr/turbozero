4b4d64456fff169699c367face6fccdeb69b15c6
lowrollr <92640744+lowrollr@users.noreply.github.com>
2023-06-26 08:49:16 -0700

    nvm conv2d does not support ints whatsoever on the gpu

--- envs/othello/torchscripts.py
+++ envs/othello/torchscripts.py
@@ -5,66 +5,67 @@ def get_legal_actions(states, ray_tensor):
     states_size = states.shape[-1]
     index = 1
     for i in range(2, states_size):
+        i_tensor = torch.tensor(i, device=states.device, dtype=torch.float32)
         start, end = 0, states_size - i 
         start_inv, end_inv = i, states_size
-        kernel_right = torch.zeros((1, 2, 1, i+1), device=states.device, requires_grad=False, dtype=torch.int8)
+        kernel_right = torch.zeros((1, 2, 1, i+1), device=states.device, requires_grad=False, dtype=torch.float32)
         kernel_right[:, 1, :, 1:i] = 1
         kernel_right[:, 0, :, i] = 1
         kernel_right[:, :, :, 0] = -1
-        ray_tensor[:, index, :, start:end] = torch.nn.functional.conv2d(states, kernel_right, padding=0).squeeze(1) == i
+        ray_tensor[:, index, :, start:end] = torch.nn.functional.conv2d(states, kernel_right, padding=0).squeeze(1).isclose(i_tensor)
         index += 1
 
-        kernel_down = torch.zeros((1, 2, i+1, 1), device=states.device, requires_grad=False, dtype=torch.int8)
+        kernel_down = torch.zeros((1, 2, i+1, 1), device=states.device, requires_grad=False, dtype=torch.float32)
         kernel_down[:, 1, 1:i, :] = 1
         kernel_down[:, 0, i, :] = 1
         kernel_down[:, :, 0, :] = -1
-        ray_tensor[:, index, start:end, :] = torch.nn.functional.conv2d(states, kernel_down, padding=0).squeeze(1) == i
+        ray_tensor[:, index, start:end, :] = torch.nn.functional.conv2d(states, kernel_down, padding=0).squeeze(1).isclose(i_tensor)
         index += 1
 
-        kernel_left = torch.zeros((1, 2, 1, i+1), device=states.device, requires_grad=False, dtype=torch.int8)
+        kernel_left = torch.zeros((1, 2, 1, i+1), device=states.device, requires_grad=False, dtype=torch.float32)
         kernel_left[:, 1, :, 1:-1] = 1
         kernel_left[:, 0, :, 0] = 1
         kernel_left[:, :, :, -1] = -1
-        ray_tensor[:, index, :, start_inv:end_inv] = torch.nn.functional.conv2d(states, kernel_left, padding=0).squeeze(1) == i
+        ray_tensor[:, index, :, start_inv:end_inv] = torch.nn.functional.conv2d(states, kernel_left, padding=0).squeeze(1).isclose(i_tensor)
         index += 1
 
-        kernel_up = torch.zeros((1, 2, i+1, 1), device=states.device, requires_grad=False, dtype=torch.int8)
+        kernel_up = torch.zeros((1, 2, i+1, 1), device=states.device, requires_grad=False, dtype=torch.float32)
         kernel_up[:, 1, 1:-1, :] = 1
         kernel_up[:, 0, 0, :] = 1
         kernel_up[:, :, -1, :] = -1
-        ray_tensor[:, index, start_inv:end_inv, :] = torch.nn.functional.conv2d(states, kernel_up, padding=0).squeeze(1) == i
+        ray_tensor[:, index, start_inv:end_inv, :] = torch.nn.functional.conv2d(states, kernel_up, padding=0).squeeze(1).isclose(i_tensor)
         index += 1
 
-        kernel_diag_right_down = torch.zeros((1, 2, i+1, i+1), device=states.device, requires_grad=False, dtype=torch.int8) 
+        kernel_diag_right_down = torch.zeros((1, 2, i+1, i+1), device=states.device, requires_grad=False, dtype=torch.float32) 
         for j in range(1, i):
             kernel_diag_right_down[:, 1, j, j] = 1
         kernel_diag_right_down[:, 0, i, i] = 1
         kernel_diag_right_down[:, :, 0, 0] = -1
-        ray_tensor[:, index, start:end, start:end] = torch.nn.functional.conv2d(states, kernel_diag_right_down, padding=0).squeeze(1) == i
+        ray_tensor[:, index, start:end, start:end] = torch.nn.functional.conv2d(states, kernel_diag_right_down, padding=0).squeeze(1).isclose(i_tensor)
         index += 1
 
-        kernel_diag_left_down = torch.zeros((1, 2, i+1, i+1), device=states.device, requires_grad=False, dtype=torch.int8)
+        kernel_diag_left_down = torch.zeros((1, 2, i+1, i+1), device=states.device, requires_grad=False, dtype=torch.float32)
         for j in range(1, i):
             kernel_diag_left_down[:, 1, j, -j-1] = 1
         kernel_diag_left_down[:, 0, i, 0] = 1
         kernel_diag_left_down[:, :, 0, -1] = -1
-        ray_tensor[:, index, start:end, start_inv:end_inv] = torch.nn.functional.conv2d(states, kernel_diag_left_down, padding=0).squeeze(1) == i
+        ray_tensor[:, index, start:end, start_inv:end_inv] = torch.nn.functional.conv2d(states, kernel_diag_left_down, padding=0).squeeze(1).isclose(i_tensor)
         index += 1
 
-        kernel_diag_left_up = torch.zeros((1, 2, i+1, i+1), device=states.device, requires_grad=False, dtype=torch.int8)
+        kernel_diag_left_up = torch.zeros((1, 2, i+1, i+1), device=states.device, requires_grad=False, dtype=torch.float32)
         for j in range(1, i):
             kernel_diag_left_up[:, 1, j, j] = 1
         kernel_diag_left_up[:, 0, 0, 0] = 1
         kernel_diag_left_up[:, :, -1, -1] = -1
-        ray_tensor[:, index, start_inv:end_inv, start_inv:end_inv] = torch.nn.functional.conv2d(states, kernel_diag_left_up, padding=0).squeeze(1) == i
+        ray_tensor[:, index, start_inv:end_inv, start_inv:end_inv] = torch.nn.functional.conv2d(states, kernel_diag_left_up, padding=0).squeeze(1).isclose(i_tensor)
         index += 1
 
-        kernel_diag_right_up = torch.zeros((1, 2, i+1, i+1), device=states.device, requires_grad=False, dtype=torch.int8)
+        kernel_diag_right_up = torch.zeros((1, 2, i+1, i+1), device=states.device, requires_grad=False, dtype=torch.float32)
         for j in range(1, i):
             kernel_diag_right_up[:, 1, -j-1, j] = 1
         kernel_diag_right_up[:, 0, 0, -1] = 1
         kernel_diag_right_up[:, :, -1, 0] = -1
-        ray_tensor[:, index, start_inv:end_inv, start:end] = torch.nn.functional.conv2d(states, kernel_diag_right_up, padding=0).squeeze(1) == i
+        ray_tensor[:, index, start_inv:end_inv, start:end] = torch.nn.functional.conv2d(states, kernel_diag_right_up, padding=0).squeeze(1).isclose(i_tensor)
         index += 1
 
     return ray_tensor.any(dim=1)
@@ -76,7 +77,7 @@ def push_actions(states, ray_tensor, actions):
     num_states = states.shape[0]
     state_indices = torch.arange(num_states, device=states.device, requires_grad=False, dtype=torch.long)
 
-    flips = torch.zeros((num_rays+1, states_size, states_size, states_size, states_size), device=states.device, requires_grad=False, dtype=torch.int8)
+    flips = torch.zeros((num_rays+1, states_size, states_size, states_size, states_size), device=states.device, requires_grad=False, dtype=torch.float32)
     f_index = 1
     for i in range(2, states_size):
         for x in range(states_size):
@@ -107,10 +108,10 @@ def push_actions(states, ray_tensor, actions):
 
     activated_rays = ray_tensor[state_indices, :, action_ys, action_xs] * (torch.arange(num_rays, device=states.device).unsqueeze(0).repeat(num_states, 1))
 
-    flips_to_apply = flips[activated_rays, action_ys.unsqueeze(1).repeat(1, 49), action_xs.unsqueeze(1).repeat(1, 49)].amax(dim=1)
+    flips_to_apply = flips[activated_rays.long(), action_ys.unsqueeze(1).repeat(1, 49), action_xs.unsqueeze(1).repeat(1, 49)].amax(dim=1)
 
     not_pass = ~states[state_indices, :, action_ys, action_xs].any(dim=1).view(-1, 1, 1)
-    states[:, 0, :, :] |= (flips_to_apply * not_pass)
-    states[:, 1, :, :] &= ~(flips_to_apply * not_pass)
-    states[state_indices, 0, action_ys, action_xs] = not_pass.view(-1).to(dtype=torch.int8)
+    states[:, 0, :, :].logical_or_(flips_to_apply * not_pass)
+    states[:, 1, :, :] *= torch.logical_not(flips_to_apply * not_pass)
+    states[state_indices, 0, action_ys, action_xs] = not_pass.view(-1).float()
     return states
