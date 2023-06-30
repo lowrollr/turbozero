ecddb49682bbe48ecdaa5bb7d0382a6cb689a6fc
lowrollr <92640744+lowrollr@users.noreply.github.com>
2023-06-29 21:31:32 -0700

    fixed ray alignment + remembered why I added the 0-th index null ray

--- envs/othello/torchscripts.py
+++ envs/othello/torchscripts.py
@@ -3,9 +3,9 @@ import torch
 
 def build_filters(device: torch.device, board_size: int):
     num_filters = (board_size - 2) * 8
-    filters = torch.zeros((num_filters, 2, board_size, board_size), dtype=torch.float32, device=device, requires_grad=False)
+    filters = torch.zeros((num_filters + 1, 2, board_size, board_size), dtype=torch.float32, device=device, requires_grad=False)
 
-    index = 0
+    index = 1
     top_left_indices = []
     top_right_indices = []
     bottom_left_indices = []
@@ -89,16 +89,16 @@ def build_filters(device: torch.device, board_size: int):
 
 def build_flips(num_rays, states_size, device):
     flips = torch.zeros((num_rays, states_size, states_size, states_size, states_size), device=device, requires_grad=False, dtype=torch.float32)
-    f_index = 0
+    f_index = 1
     for i in range(2, states_size):
         for x in range(states_size):
             for y in range(states_size):
-                # right, down, left, up
+                # right, left, down, up
                 if x+1 < states_size:
                     flips[f_index, y, x, y, x+1:min(x+i, states_size)] = 1
+                flips[f_index+1, y, x, y, max(x-i, 0):x] = 1
                 if y+1 < states_size:
-                    flips[f_index+1, y, x, y+1:min(y+i, states_size), x] = 1
-                flips[f_index+2, y, x, y, max(x-i, 0):x] = 1
+                    flips[f_index+2, y, x, y+1:min(y+i, states_size), x] = 1
                 flips[f_index+3, y, x, max(y-i, 0):y, x] = 1
 
                 # diag right down, diag left down, diag left up, diag right up
@@ -107,12 +107,12 @@ def build_flips(num_rays, states_size, device):
                         if x+j < states_size:
                             flips[f_index+4, y, x, y+j, x+j] = 1
                         if x-j >= 0:
-                            flips[f_index+5, y, x, y+j, x-j] = 1
+                            flips[f_index+7, y, x, y+j, x-j] = 1
                     if y-j >= 0:
                         if x-j >= 0:
                             flips[f_index+6, y, x, y-j, x-j] = 1
                         if x+j < states_size:
-                            flips[f_index+7, y, x, y-j, x+j] = 1
+                            flips[f_index+5, y, x, y-j, x+j] = 1
         f_index += 8
     return flips
 
