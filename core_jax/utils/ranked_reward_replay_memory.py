1e5079ad5fa1cfe90525a353ca1588db1563f15d
lowrollr <92640744+lowrollr@users.noreply.github.com>
2023-11-25 12:01:33 -0800

    ranked reward fixes

--- core_jax/utils/ranked_reward_replay_memory.py
+++ core_jax/utils/ranked_reward_replay_memory.py
@@ -10,6 +10,7 @@ from core_jax.utils.replay_memory import EndRewardReplayBuffer, EndRewardReplayB
 class RankedRewardReplayBufferState(EndRewardReplayBufferState):
     next_index: jnp.ndarray # next index to write experience to
     next_reward_index: jnp.ndarray # next index to write reward to
+    next_raw_reward_index: jnp.ndarray # next index to write raw reward to
     buffer: struct.PyTreeNode # buffer of experiences
     reward_buffer: jnp.ndarray # buffer of ranked rewards
     raw_reward_buffer: jnp.ndarray # buffer of raw rewards
@@ -22,55 +23,58 @@ class RankedRewardReplayBuffer(EndRewardReplayBuffer):
         batch_size: int,
         max_len_per_batch: int,
         sample_batch_size: int,
-        percentile: int = 75
+        episode_reward_memory_len_per_batch: int,
+        quantile: float = 0.75,
     ):
         super().__init__(batch_size, max_len_per_batch, sample_batch_size)
-        self.percentile = percentile
+        self.quantile = quantile
+        self.episode_reward_memory_len_per_batch = episode_reward_memory_len_per_batch
 
     def init(self, template_experience: struct.PyTreeNode) -> EndRewardReplayBufferState:
-        return init(template_experience, self.batch_size, self.max_len_per_batch)
+        return init(template_experience, self.batch_size, self.max_len_per_batch, self.episode_reward_memory_len_per_batch)
 
     def assign_rewards(self, state: EndRewardReplayBufferState, rewards: jnp.ndarray, select_batch: jnp.ndarray) -> EndRewardReplayBufferState:
-        return assign_rewards(state, rewards, select_batch.astype(jnp.bool_), self.max_len_per_batch, self.percentile)
+        return assign_rewards(state, rewards, select_batch.astype(jnp.bool_), self.max_len_per_batch, self.quantile, self.episode_reward_memory_len_per_batch)
 
 
 
-@partial(jax.jit, static_argnums=(1,2))
+@partial(jax.jit, static_argnums=(1,2,3))
 def init(
     template_experience: struct.PyTreeNode,
     batch_size: int,
     max_len_per_batch: int,
+    episode_reward_memory_len_per_batch: int,
 ) -> RankedRewardReplayBufferState:
     buffer_state = super_init(template_experience, batch_size, max_len_per_batch)
     return RankedRewardReplayBufferState(
         **buffer_state.__dict__, 
-        raw_reward_buffer=jnp.zeros((batch_size, max_len_per_batch, 1))
+        raw_reward_buffer=jnp.zeros((batch_size, episode_reward_memory_len_per_batch, 1)),
+        next_raw_reward_index=jnp.zeros((batch_size,), dtype=jnp.int32)
     )
     
 
 
-@partial(jax.jit, static_argnums=(3,4))
+@partial(jax.jit, static_argnums=(3,4,5))
 def assign_rewards(
     buffer_state: RankedRewardReplayBufferState,
     rewards: jnp.ndarray,
     select_batch: jnp.ndarray,
     max_len_per_batch: int,
-    percentile: int
+    quantile: float,
+    episode_reward_memory_len_per_batch: int,
 ) -> RankedRewardReplayBufferState:
     rand_key, new_key = jax.random.split(buffer_state.key)
     rand_bools = jax.random.uniform(rand_key, rewards.shape) < 0.5
-    percentile_value = jax.vmap(jnp.percentile, in_axes=(0,None,None))(
-        buffer_state.raw_reward_buffer, 
-        percentile, 
-        1
-    ).mean()
-    
+
+    quantile_value = jnp.quantile(buffer_state.raw_reward_buffer, quantile, axis=-1).mean()
+
+
     def rank_rewards(reward, boolean):
         return jnp.where(
-            reward < percentile_value, 
+            reward < quantile_value, 
             -1,
             jnp.where(
-                reward > percentile_value,
+                reward > quantile_value,
                 1,
                 jnp.where(
                     boolean,
@@ -80,27 +84,27 @@ def assign_rewards(
             )
         )
     
-    
-    ranked_rewards = jax.vmap(rank_rewards)(rewards, rand_bools)
-    
-    rewards = jax.vmap(jnp.roll, in_axes=(0, 0))(rewards, buffer_state.next_reward_index)
-    rewards = jnp.tile(rewards, (1, max_len_per_batch // rewards.shape[-1]))
+    ranked_rewards = rank_rewards(rewards, rand_bools)
 
     ranked_rewards = jax.vmap(jnp.roll, in_axes=(0, 0))(ranked_rewards, buffer_state.next_reward_index)
     ranked_rewards = jnp.tile(ranked_rewards, (1, max_len_per_batch // ranked_rewards.shape[-1]))
     
     
+    
     return buffer_state.replace(
         key=new_key,
+        next_raw_reward_index = (buffer_state.next_raw_reward_index + 1) % episode_reward_memory_len_per_batch,
         reward_buffer = jnp.where(
             select_batch[..., None, None] & buffer_state.needs_reward,
             ranked_rewards[..., None],
             buffer_state.reward_buffer
         ),
-        raw_reward_buffer = jnp.where(
-            select_batch[..., None, None] & buffer_state.needs_reward,
-            rewards[..., None],
-            buffer_state.raw_reward_buffer
+        raw_reward_buffer = buffer_state.raw_reward_buffer.at[:, buffer_state.next_raw_reward_index, 0].set(
+            jnp.where(
+                select_batch,
+                rewards,
+                buffer_state.raw_reward_buffer[:, buffer_state.next_raw_reward_index, 0]
+            )    
         ),
         next_reward_index = jnp.where(
             select_batch,
