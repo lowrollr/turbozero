87054f6cfae16c39e34fa32d4fab179168813576
lowrollr <92640744+lowrollr@users.noreply.github.com>
2024-03-21 11:57:11 -0700

    fix render bugs

--- core/testing/utils.py
+++ core/testing/utils.py
@@ -9,9 +9,9 @@ from PIL import Image
 def render_pgx_2p(frames, p_ids, title, frame_dir, p1_label='black', p2_label='white', duration=900):
     digit_length = len(str(len(frames)))
     trained_agent_color = p1_label if p_ids[0] == 0 else p2_label
-    opponent_color = p2_label if trained_agent_color == p1_label else p2_label
-    p1_win = False
-    p2_win = False
+    opponent_color = p2_label if trained_agent_color == p1_label else p1_label
+    agent_win = False
+    opp_win = False
     draw = False
     images = []
     for i,frame in enumerate(frames):
@@ -19,8 +19,8 @@ def render_pgx_2p(frames, p_ids, title, frame_dir, p1_label='black', p2_label='w
         if frame.completed.item():
             num = '9' * digit_length
             env_state.save_svg(f"{frame_dir}/{num}.svg", color_theme='dark')
-            p1_win = frame.outcomes[0] > frame.outcomes[1]
-            p2_win = frame.outcomes[1] > frame.outcomes[0]
+            agent_win = frame.outcomes[p_ids[0]] > frame.outcomes[p_ids[1]]
+            opp_win = frame.outcomes[p_ids[1]] > frame.outcomes[p_ids[0]]
             draw = frame.outcomes[0] == frame.outcomes[1]
     
         else:
@@ -50,9 +50,9 @@ def render_pgx_2p(frames, p_ids, title, frame_dir, p1_label='black', p2_label='w
         # Create a new text element
         p1_text = ET.Element('ns0:text', x=str(0.01 * original_width), y=str(original_height * 1.05), fill='white', style='font-family: Arial;')
         p2_text = ET.Element('ns0:text', x=str(0.01 * original_width), y=str(original_height * 1.15), fill='white', style='font-family: Arial;')
-        emoji = "[W]" if p1_win else "[L]" if p2_win else "[D]" if draw else ""
+        emoji = "[W]" if agent_win else "[L]" if opp_win else "[D]" if draw else ""
         agent_text = f"{emoji} Trained Agent ({trained_agent_color}): {'+' if frame.p1_value_estimate > 0 else ''}{frame.p1_value_estimate:.4f}"
-        emoji = "[W]" if p2_win else "[L]" if p1_win else "[D]" if draw else ""
+        emoji = "[W]" if opp_win else "[L]" if agent_win else "[D]" if draw else ""
         opp_text = f"{emoji} Opponent ({opponent_color}): {'+' if frame.p2_value_estimate > 0 else ''}{frame.p2_value_estimate:.4f}"
         p1_text.text = agent_text
         p2_text.text = opp_text
@@ -60,13 +60,9 @@ def render_pgx_2p(frames, p_ids, title, frame_dir, p1_label='black', p2_label='w
         new_area = ET.Element('ns0:rect', fill='#1e1e1e', height=str(new_height - original_height), width=str(original_width), x='0', y=str(original_height))
         root.append(new_area)
 
-        # Add the text element to the SVG
         root.append(p1_text)
         root.append(p2_text)
     
-        
-
-        # Save the modified SVG
         tree.write(f"{frame_dir}/{num}.svg", encoding='utf-8', xml_declaration=True)
 
         cairosvg.svg2png(url=f"{frame_dir}/{num}.svg", write_to=f"{frame_dir}/{num}.png")
@@ -75,10 +71,8 @@ def render_pgx_2p(frames, p_ids, title, frame_dir, p1_label='black', p2_label='w
             break
 
 
-    # Read the PNG images into a list of Image objects
     images = [Image.open(png) for png in images]
 
-    # Save the images as a GIF
     gif_path = f"{frame_dir}/{title}.gif"
     images[0].save(gif_path, save_all=True, append_images=images[1:] + ([images[-1]] * 2), duration=duration, loop=0)
     
