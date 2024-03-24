dc28d9c57025e63b0702be7fbff86c51e1547575
lowrollr <92640744+lowrollr@users.noreply.github.com>
2024-03-24 13:18:04 -0700

    two_player_tester docstrings

--- core/testing/two_player_tester.py
+++ core/testing/two_player_tester.py
@@ -1,44 +1,78 @@
 
-
-
-
 from functools import partial
-from typing import Any, Dict, Optional, Tuple
-from chex import dataclass
+from typing import Dict, Tuple
+
 import chex
+from chex import dataclass
 import jax
+
 from core.common import two_player_game
 from core.evaluators.evaluator import Evaluator
 from core.testing.tester import BaseTester, TestState
 from core.types import EnvInitFn, EnvStepFn
 
+
 @dataclass(frozen=True)
 class TwoPlayerTestState(TestState):
+    """Internal state of a TwoPlayerTester. Stores the best parameters found so far.
+    - `best_params`: best performing parameters
+    """
     best_params: chex.ArrayTree
 
+
 class TwoPlayerTester(BaseTester):
+    """Implements a tester that evaluates an agent against the best performing parameters 
+    found so far in a two-player game."""
     def __init__(self, num_episodes: int, *args, **kwargs):
+        """
+        Args:
+        - `num_episodes`: number of episodes to play in each test
+        """
         super().__init__(*args, **kwargs)
         self.num_episodes = num_episodes
 
-    def init(self, params: chex.ArrayTree, **kwargs) -> TwoPlayerTestState:
+
+    def init(self, params: chex.ArrayTree, **kwargs) -> TwoPlayerTestState: #pylint: disable=unused-argument
+        """Initializes the internal state of the TwoPlayerTester.
+        Args:
+        - `params`: initial parameters to store as the best performing
+            - can just be the initial parameters of the agent
+        """
         return TwoPlayerTestState(best_params=params)
     
+
     def check_size_compatibilities(self, num_devices: int) -> None:
+        """Checks if tester configuration is compatible with number of devices being utilized.
+        
+        Args:
+        - `num_devices`: number of devices
+        """
         if self.num_episodes % num_devices != 0:
             raise ValueError(f"{self.__class__.__name__}: number of episodes ({self.num_episodes}) must be divisible by number of devices ({num_devices})")
 
-    @partial(jax.pmap, axis_name='d', static_broadcasted_argnums=(0, 1, 2, 3, 4))
-    def test(self,
-        max_steps: int,
-        env_step_fn: EnvStepFn,
-        env_init_fn: EnvInitFn,
-        evaluator: Evaluator,
-        keys: chex.PRNGKey,
-        state: TwoPlayerTestState,
-        params: chex.ArrayTree
-    ) -> Tuple[TwoPlayerTestState, Dict, chex.ArrayTree]:
 
+    @partial(jax.pmap, axis_name='d', static_broadcasted_argnums=(0, 1, 2, 3, 4))
+    def test(self, max_steps: int, env_step_fn: EnvStepFn, env_init_fn: EnvInitFn, evaluator: Evaluator,
+        keys: chex.PRNGKey, state: TwoPlayerTestState, params: chex.ArrayTree) -> Tuple[TwoPlayerTestState, Dict, chex.ArrayTree, chex.Array]:
+        """Test the agent against the best performing parameters found so far in a two-player game.
+        
+        Args:
+        - `max_steps`: maximum number of steps per episode
+        - `env_step_fn`: environment step function
+        - `env_init_fn`: environment initialization function
+        - `evaluator`: the agent evaluator
+        - `keys`: rng
+        - `state`: internal state of the tester
+        - `params`: nn parameters used by agent
+        
+        Returns:
+        - (TwoPlayerTestState, Dict, chex.ArrayTree, chex.Array)
+            - updated internal state of the tester
+            - metrics from the test
+            - frames from the test (used for rendering)
+            - player ids from the test (used for rendering)
+        """
+        
         game_fn = partial(two_player_game,
             evaluator_1 = evaluator,
             evaluator_2 = evaluator,
