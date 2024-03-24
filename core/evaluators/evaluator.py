b395fdeb32bd168022ba29094d2061a03975181e
lowrollr <92640744+lowrollr@users.noreply.github.com>
2024-03-23 18:26:32 -0700

    evaluator docstrings

--- core/evaluators/evaluator.py
+++ core/evaluators/evaluator.py
@@ -1,38 +1,97 @@
 
 from typing import Dict
+
 import chex
-from chex import dataclass
 import jax
 import jax.numpy as jnp
+from chex import dataclass
+
 
 @dataclass(frozen=True)
 class EvalOutput:
+    """Output of an evaluation.
+    - `eval_state`: The updated internal state of the Evaluator.
+    - `action`: The action to take.
+    - `policy_weights`: The policy weights assigned to each action.
+    """
     eval_state: chex.ArrayTree
     action: int
     policy_weights: chex.Array
 
+
 class Evaluator:
-    def __init__(self, discount: float, *args, **kwargs):
+    """Base class for Evaluators.
+    An Evaluator *evaluates* an environment state, and returns an action to take, as well as a 'policy', assigning a weight to each action.
+    Evaluators may maintain an internal state, which is updated by the `step` method.
+    """
+
+    def __init__(self, discount: float, *args, **kwargs):  # pylint: disable=unused-argument
+        """Initializes an Evaluator.
+
+        Args:
+        - `discount`: The discount factor applied to future rewards/value estimates.
+        """
         self.discount = discount
 
+
     def init(self, *args, **kwargs) -> chex.ArrayTree:
+        """Initializes the internal state of the Evaluator."""
         raise NotImplementedError()
-    
-    def init_batched(self, batch_size: int, *args, **kwargs) -> chex.ArrayTree: 
-        tree = self.init(*args, **kwargs) 
+
+
+    def init_batched(self, batch_size: int, *args, **kwargs) -> chex.ArrayTree:
+        """Initializes the internal state of the Evaluator across a batch dimension."""
+        tree = self.init(*args, **kwargs)
         return jax.tree_map(lambda x: jnp.broadcast_to(x, (batch_size,) + x.shape), tree)
 
+
     def reset(self, state: chex.ArrayTree) -> chex.ArrayTree:
-        raise NotImplementedError()
-    
-    def evaluate(self, key: chex.PRNGKey, eval_state: chex.ArrayTree, env_state: chex.ArrayTree, **kwargs) -> EvalOutput:
+        """Resets the internal state of the Evaluator."""
         raise NotImplementedError()
 
-    def step(self, state: chex.ArrayTree, action: chex.Array) -> chex.ArrayTree:
-        return state
-    
-    def get_value(self, state: chex.ArrayTree) -> chex.Array:
+
+    def evaluate(self, key: chex.PRNGKey, eval_state: chex.ArrayTree, env_state: chex.ArrayTree, **kwargs) -> EvalOutput:
+        """Evaluates the environment state.
+
+        Args:
+        - `key`: rng
+        - `eval_state`: The internal state of the Evaluator.
+        - `env_state`: The environment state to evaluate.
+
+        Returns:
+        - `EvalOutput`: The output of the evaluation.
+            - `eval_state`: The updated internal state of the Evaluator.
+            - `action`: The action to take.
+            - `policy_weights`: The policy weights assigned to each action.
+        """
         raise NotImplementedError()
-    
+
+
+    def step(self, state: chex.ArrayTree, action: chex.Array) -> chex.ArrayTree:  # pylint: disable=unused-argument
+        """Updates the internal state of the Evaluator.
+
+        Args:
+        - `state`: The internal state of the Evaluator.
+        - `action`: The action taken in the environment.
+
+        Returns:
+        - (chex.ArrayTree): The updated internal state of the Evaluator.
+        """
+        return state
+
+
+    def get_value(self, state: chex.ArrayTree) -> chex.Array:
+        """Extracts the state value estimate (for the current/root environment state) from the internal state of the Evaluator.
+
+        Args:
+        - `state`: The internal state of the Evaluator.
+
+        Returns:
+        - `chex.Array`: The value estimate.
+        """
+        raise NotImplementedError()
+
+
     def get_config(self) -> Dict:
-        return {}
+        """Returns the configuration of the Evaluator. Used for logging."""
+        return {'discount': self.discount}
